7fc5a49d130583097626fa3a7acbfbda4a505578
yiqiao <yimingqiao3163@gmail.com>
2024-01-08 18:40:28 +0800

    Create base.h and update code organization
    Introduced a new file, base.h, to help consolidate common constants and data type definitions. Moved kBlockSize and Attribute definitions from main.cpp to this new header file. Also, updated .clang-format and .clang-tidy files for code style and linting rules, respectively. Updated CMakeLists.txt to include base.h in the project build.

--- main.cpp
+++ main.cpp
@@ -3,17 +3,7 @@
 #include <variant>
 #include <random>
 
-constexpr size_t kBlockSize = 2048;
-
-using Attribute = std::variant<size_t, double, std::string>;
-
-
-
-
-std::vector<std::vector<Attribute>> GenerateTable() {
-    std::vector<std::vector<Attribute>> table(5e7);
-    return table;
-}
+#include "base.h"
 
 int main() {
 
