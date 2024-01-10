21eec5ce625d3e36bd32e5c3ccf5493f6ec4ecf3
yiqiao <yimingqiao3163@gmail.com>
2024-01-10 20:04:24 +0800

    Increase LHSTupleSize and RHSTupleSize; revise the main.cpp structure
    The size of LHSTuple and RHSTuple were significantly increased to enhance the system's performance and capacity. Main.cpp has been refactored to introduce an encapsulated function profiler for better computation time tracking. Additionally, the blocksize chunk processing and join operations were modified for speed optimization and detailed time logging.

--- main.cpp
+++ main.cpp
@@ -8,24 +8,22 @@
 
 using namespace compaction;
 
-const size_t kLHSTupleSize = 32000;
-const size_t kRHSTupleSize = 32000;
-const size_t kChunkFactor = 3;
+const size_t kLHSTupleSize = 5e6;
+const size_t kRHSTupleSize = 5e5;
+const size_t kChunkFactor = 1;
 
 int main() {
   // random generator
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_int_distribution<> age_dist(18, 100);
-  std::uniform_int_distribution<> course_id_dist(0, kRHSTupleSize);
-  std::uniform_int_distribution<> major_id_dist(0, kRHSTupleSize);
+  std::uniform_int_distribution<> dist(0, kRHSTupleSize);
 
   // create probe table: (id, course_id, major_id, miscellaneous)
   vector<AttributeType> types
       {AttributeType::INTEGER, AttributeType::INTEGER, AttributeType::INTEGER, AttributeType::STRING};
   compaction::DataCollection table(types);
   for (size_t i = 0; i < kLHSTupleSize; ++i) {
-    std::vector<compaction::Attribute> tuple{i, size_t(course_id_dist(gen)), size_t(major_id_dist(gen)), "|"};
+    std::vector<compaction::Attribute> tuple{size_t(dist(gen)), size_t(dist(gen)), size_t(dist(gen)), "|"};
     table.AppendTuple(tuple);
   }
 
@@ -48,43 +46,68 @@ int main() {
   // create the result_table collection
   DataCollection result_table(types);
 
-  // Start process each chunk in the lhs table
-  size_t start = 0;
-  size_t end;
-  do {
-    end = std::min(start + kBlockSize, kLHSTupleSize);
-    DataChunk chunk = table.FetchChunk(start, end);
-    start = end;
+  {
+    Profiler profiler, profiler_join_1, profiler_join_2, profiler_join_3;
+    profiler.Start();
 
-    // Join: 1
-    Vector &join_key = chunk.data_[0];
-    auto ss = ht_1.Probe(join_key);
+    // Start process each chunk in the lhs table
+    size_t start = 0;
+    size_t end;
+    do {
+      end = std::min(start + kBlockSize, kLHSTupleSize);
+      DataChunk chunk = table.FetchChunk(start, end);
+      start = end;
 
-    while (ss.HasNext()) {
-      ss.Next(join_key, chunk, result_chunk_1);
+      profiler_join_1.Start();
 
-      // Join: 2
-      Vector &join_key = result_chunk_1.data_[1];
-      auto ss = ht_2.Probe(join_key);
+      // Join: 1
+      Vector &join_key = chunk.data_[0];
+      auto ss = ht_1.Probe(join_key);
 
       while (ss.HasNext()) {
-        ss.Next(join_key, result_chunk_1, result_chunk_2);
+        ss.Next(join_key, chunk, result_chunk_1);
 
-        // Join: 3
-        Vector &join_key = result_chunk_2.data_[2];
-        auto ss = ht_3.Probe(join_key);
+        profiler_join_2.Start();
+
+        // Join: 2
+        Vector &join_key = result_chunk_1.data_[1];
+        auto ss = ht_2.Probe(join_key);
 
         while (ss.HasNext()) {
-          ss.Next(join_key, result_chunk_2, result_chunk_3);
-          result_table.AppendChunk(result_chunk_3);
+          ss.Next(join_key, result_chunk_1, result_chunk_2);
+
+          profiler_join_3.Start();
+
+          // Join: 3
+          Vector &join_key = result_chunk_2.data_[2];
+          auto ss = ht_3.Probe(join_key);
+
+          while (ss.HasNext()) {
+            ss.Next(join_key, result_chunk_2, result_chunk_3);
+            // result_table.AppendChunk(result_chunk_3);
+          }
+
+          double time = profiler_join_3.Elapsed();
+          ZebraProfiler::Get().InsertRecord("3rd_join_IN", result_chunk_2.count_, time);
         }
+
+        double time = profiler_join_2.Elapsed();
+        ZebraProfiler::Get().InsertRecord("2nd_join_IN", result_chunk_1.count_, time);
       }
-    }
-  } while (end < kLHSTupleSize);
+
+      double time = profiler_join_1.Elapsed();
+      ZebraProfiler::Get().InsertRecord("1st_join_IN", chunk.count_, time);
+    } while (end < kLHSTupleSize);
+
+    double latency = profiler.Elapsed();
+    std::cout << "[Total Time]: " << latency << "s\n";
+  }
+
+  ZebraProfiler::Get().ToCSV();
 
   // show the joined result.
-  std::cout << "Number of tuples in the result table: " << result_table.NumTuples() << "\n";
-  result_table.Print(10);
+//  std::cout << "Number of tuples in the result table: " << result_table.NumTuples() << "\n";
+//  result_table.Print(10);
 
   return 0;
 }
