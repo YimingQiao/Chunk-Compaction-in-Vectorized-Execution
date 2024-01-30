cb26829493b1e5c3df847b27a340e934cb8fb083
yiqiao <yimingqiao3163@gmail.com>
2024-01-30 16:52:11 +0800

    Modify query and compaction settings, streamline pipeline functions
    Removed const qualifier from size_t variables and modified the flag_dynamic_compact value in settings.h. In main.cpp, removed excessive functions in ExecutePipeline and main. Integrated parameter parsing, pipeline execution, and rendering of settings and statistics. These changes aim to simplify the code base and optimize runtime operations.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <cstring>
 
 #include "base.h"
 #include "hash_table.h"
@@ -18,7 +19,157 @@ struct PipelineState {
   PipelineState() : hts(kJoins), intermediates(kJoins), compactors(kJoins) {}
 };
 
-static void ExecutePipeline(DataChunk &input, PipelineState &state, DataCollection &result_table, size_t level) {
+static void ExecutePipeline(DataChunk &input, PipelineState &state, DataCollection &result_table, size_t level);
+
+void FlushPipelineCache(PipelineState &state, DataCollection &result_table, size_t level);
+
+std::vector<size_t> ParseList(const std::string &s) {
+  std::stringstream ss(s.substr(1, s.size() - 2)); // Ignore brackets
+  std::vector<size_t> result;
+  std::string item;
+  while (std::getline(ss, item, ',')) {
+    result.push_back(std::stoi(item));
+  }
+  return result;
+}
+
+void ParseParameters(int argc, char *argv[]) {
+  if (argc != 1 && argc != 5) throw std::runtime_error("Incomplete Parameters");
+
+  if (argc == 5) {
+    for (int i = 1; i < argc; i++) {
+      std::string arg(argv[i]);
+
+      if (arg == "--join-num") {
+        if (i + 1 < argc) {
+          kJoins = std::stoi(argv[i + 1]);
+          i++;
+        }
+      } else if (arg == "--chunk-factor") {
+        if (i + 1 < argc) {
+          kChunkFactor = std::stoi(argv[i + 1]);
+          i++;
+        }
+      } else if (arg == "--lhs_size") {
+        if (i + 1 < argc) {
+          kLHSTupleSize = std::stoi(argv[i + 1]);
+          i++;
+        }
+      } else if (arg == "--rhs_size") {
+        if (i + 1 < argc) {
+          kRHSTupleSize = std::stoi(argv[i + 1]);
+          i++;
+        }
+      } else if (arg.substr(0, 16) == "--payload-length") {
+        // --payload-length=[0,1000,0,0]
+        kRHSPayLoadLength = ParseList(arg.substr(17));
+      }
+    }
+
+    if (kJoins != kRHSPayLoadLength.size())
+      throw std::runtime_error("Payload vector length must equal to the number of joins.");
+  }
+
+  // show the setting
+  std::cerr << "------------------ Setting ------------------\n";
+  std::cerr << "Number of Joins: " << kJoins << "\n"
+            << "Number of LHS Tuple: " << kLHSTupleSize << "\n"
+            << "Number of RHS Tuple: " << kRHSTupleSize << "\n";
+  std::cerr << "RHS Payload Lengths: [";
+  for (size_t i = 0; i < kJoins; ++i) {
+    if (i != kJoins - 1) std::cerr << kRHSPayLoadLength[i] << ",";
+    else std::cerr << kRHSPayLoadLength[i] << "]\n";
+  }
+}
+
+int main(int argc, char *argv[]) {
+  ParseParameters(argc, argv);
+
+  // random generator
+  std::random_device rd;
+  std::mt19937 gen(2);
+  std::uniform_int_distribution<> dist(0, kRHSTupleSize);
+
+  // ---------------------------------------------- Query Setting ----------------------------------------------
+
+  // create probe table: (id1, id2, ..., idn, miscellaneous)
+  vector<AttributeType> types;
+  for (size_t i = 0; i < kJoins; ++i) types.push_back(AttributeType::INTEGER);
+  types.push_back(AttributeType::STRING);
+  compaction::DataCollection table(types);
+  vector<compaction::Attribute> tuple(kJoins + 1);
+  tuple[kJoins] = "|";
+  for (size_t i = 0; i < kLHSTupleSize; ++i) {
+    for (size_t j = 0; j < kJoins; ++j) tuple[j] = size_t(dist(gen));
+    table.AppendTuple(tuple);
+  }
+
+  // create rhs hash table, and the result_table chunk
+  PipelineState state;
+  auto &hts = state.hts;
+  auto &intermediates = state.intermediates;
+  auto &compactors = state.compactors;
+  for (size_t i = 0; i < kJoins; ++i) {
+    hts[i] = std::make_unique<HashTable>(kRHSTupleSize, kChunkFactor, kRHSPayLoadLength[i]);
+    types.push_back(AttributeType::INTEGER);
+    types.push_back(AttributeType::STRING);
+    intermediates[i] = std::make_unique<DataChunk>(types);
+    compactors[i] = std::make_unique<Compactor>(types);
+  }
+
+  // create the result_table collection
+  DataCollection result_table(types);
+
+#ifdef flag_dynamic_compact
+  // create MAB for each join operator
+  for (size_t i = 0; i < kJoins; ++i) CompactTuner::Get().Initialize(size_t(&state.compactors[i]));
+#endif
+  // -----------------------------------------------------------------------------------------------------------
+
+  double latency = 0;
+  Profiler timer;
+  {
+    // Start process each chunk in the lhs table
+    size_t num_chunk_size = kBlockSize;
+    size_t start = 0;
+    size_t end;
+    do {
+      end = std::min(start + num_chunk_size, kLHSTupleSize);
+      // num_chunk_size = (num_chunk_size + 1) % kBlockSize;
+      DataChunk chunk = table.FetchChunk(start, end);
+      start = end;
+
+      timer.Start();
+      ExecutePipeline(chunk, state, result_table, 0);
+      latency += timer.Elapsed();
+    } while (end < kLHSTupleSize);
+
+#if defined(flag_full_compact) || defined(flag_binary_compact) || defined(flag_dynamic_compact)
+    timer.Start();
+    {
+      // Flush the tuples in cache.
+      FlushPipelineCache(state, result_table, 0);
+    }
+    latency += timer.Elapsed();
+#endif
+  }
+
+  std::cerr << "------------------ Statistic ------------------\n";
+  std::cerr << "[Total Time]: " << latency << "s\n";
+  BeeProfiler::Get().EndProfiling();
+  ZebraProfiler::Get().ToCSV();
+  CompactTuner::Get().Reset();
+
+#ifdef flag_collect_tuples
+  // show the joined result.
+  std::cout << "Number of tuples in the result table: " << result_table.NumTuples() << "\n";
+  result_table.Print(8);
+#endif
+
+  return 0;
+}
+
+void ExecutePipeline(DataChunk &input, PipelineState &state, DataCollection &result_table, size_t level) {
   auto &hts = state.hts;
   auto &intermediates = state.intermediates;
   auto &compactors = state.compactors;
@@ -92,90 +243,5 @@ void FlushPipelineCache(PipelineState &state, DataCollection &result_table, size
 
   // Flush the next level.
   FlushPipelineCache(state, result_table, level + 1);
-}
-
-int main() {
-  // random generator
-  std::random_device rd;
-  std::mt19937 gen(2);
-  std::uniform_int_distribution<> dist(0, kRHSTupleSize);
-
-  // ---------------------------------------------- Query Setting ----------------------------------------------
-
-  // create probe table: (id1, id2, ..., idn, miscellaneous)
-  vector<AttributeType> types;
-  for (size_t i = 0; i < kJoins; ++i) types.push_back(AttributeType::INTEGER);
-  types.push_back(AttributeType::STRING);
-  compaction::DataCollection table(types);
-  vector<compaction::Attribute> tuple(kJoins + 1);
-  tuple[kJoins] = "|";
-  for (size_t i = 0; i < kLHSTupleSize; ++i) {
-    for (size_t j = 0; j < kJoins; ++j) tuple[j] = size_t(dist(gen));
-    table.AppendTuple(tuple);
-  }
-
-  // create rhs hash table, and the result_table chunk
-  PipelineState state;
-  auto &hts = state.hts;
-  auto &intermediates = state.intermediates;
-  auto &compactors = state.compactors;
-  for (size_t i = 0; i < kJoins; ++i) {
-    hts[i] = std::make_unique<HashTable>(kRHSTupleSize, kChunkFactor, kRHSPayLoadLength[i]);
-    types.push_back(AttributeType::INTEGER);
-    types.push_back(AttributeType::STRING);
-    intermediates[i] = std::make_unique<DataChunk>(types);
-    compactors[i] = std::make_unique<Compactor>(types);
-  }
-
-  // create the result_table collection
-  DataCollection result_table(types);
-
-#ifdef flag_dynamic_compact
-  // create MAB for each join operator
-  for (size_t i = 0; i < kJoins; ++i) CompactTuner::Get().Initialize(size_t(&state.compactors[i]));
-#endif
-  // -----------------------------------------------------------------------------------------------------------
-
-  double latency = 0;
-  Profiler timer;
-  {
-    // Start process each chunk in the lhs table
-    size_t num_chunk_size = kBlockSize;
-    size_t start = 0;
-    size_t end;
-    do {
-      end = std::min(start + num_chunk_size, kLHSTupleSize);
-      // num_chunk_size = (num_chunk_size + 1) % kBlockSize;
-      DataChunk chunk = table.FetchChunk(start, end);
-      start = end;
-
-      timer.Start();
-      ExecutePipeline(chunk, state, result_table, 0);
-      latency += timer.Elapsed();
-    } while (end < kLHSTupleSize);
-
-#if defined(flag_full_compact) || defined(flag_binary_compact) || defined(flag_dynamic_compact)
-    timer.Start();
-    {
-      // Flush the tuples in cache.
-      FlushPipelineCache(state, result_table, 0);
-    }
-    latency += timer.Elapsed();
-#endif
-
-    std::cout << "[Total Time]: " << latency << "s\n";
-  }
-
-  BeeProfiler::Get().EndProfiling();
-  ZebraProfiler::Get().ToCSV();
-  CompactTuner::Get().Reset();
-
-#ifdef flag_collect_tuples
-  // show the joined result.
-  std::cout << "Number of tuples in the result table: " << result_table.NumTuples() << "\n";
-  result_table.Print(8);
-#endif
-
-  return 0;
 }
 
--- setting.h
+++ setting.h
@@ -13,14 +13,14 @@
 namespace compaction {
 
 // query setting
-const size_t kJoins = 4;
-const size_t kLHSTupleSize = 2e7;
-const size_t kRHSTupleSize = 2e6;
-const size_t kChunkFactor = 6;
-const vector<size_t> kRHSPayLoadLength{0, 1000, 0, 0};
+size_t kJoins = 4;
+size_t kLHSTupleSize = 2e7;
+size_t kRHSTupleSize = 2e6;
+size_t kChunkFactor = 6;
+vector<size_t> kRHSPayLoadLength{0, 0, 0, 0};
 
 // compaction setting
-#define flag_dynamic_compact
+#define flag__compact
 
 #ifdef flag_full_compact
 using Compactor = NaiveCompactor;
