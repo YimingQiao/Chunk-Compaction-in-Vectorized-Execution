51e6a4f00e70a75030b48a1fd5a6cfd2203e8885
yiqiao <yimingqiao3163@gmail.com>
2024-01-30 15:31:44 +0800

    Update settings for HashTable constructor and compaction
    The HashTable constructor now supports variable payload lengths with the addition of a `payload_length` parameter. The compaction settings have been adjusted to handle different compactor types, simpl

--- setting.h
+++ setting.h
@@ -14,10 +14,10 @@ namespace compaction {
 
 // query setting
 const size_t kJoins = 4;
+const vector<size_t> kRHSPayLoadLength{0, 1000, 0, 0};
 const size_t kLHSTupleSize = 2e7;
 const size_t kRHSTupleSize = 2e6;
-const size_t kChunkFactor = 6;
-const vector<size_t> kRHSPayLoadLength{0, 1000, 0, 0};
+const size_t kChunkFactor = 1;
 
 // compaction setting
 #define flag_no_compact
