3fa81b76f72c1b36b1c8875f3e3b14cf086d2420
yiqiao <yimingqiao3163@gmail.com>
2024-01-31 16:01:28 +0800

    Refine compaction strategy naming conventions
    This change updates the strategy_name constants in setting.h to more accurately reflect common naming conventions. Specifically, the terms "compact" are replaced with "compaction" for better clarity and code readability.

--- setting.h
+++ setting.h
@@ -23,16 +23,16 @@ vector<size_t> kRHSPayLoadLength{0, 0, 0, 0};
 
 #ifdef flag_full_compact
 using Compactor = NaiveCompactor;
-const string strategy_name = "full_compact";
+const string strategy_name = "full_compaction";
 #elif defined(flag_binary_compact)
 using Compactor = BinaryCompactor;
-const string strategy_name = "binary_compact";
+const string strategy_name = "binary_compaction";
 #elif defined(flag_dynamic_compact)
 using Compactor = DynamicCompactor;
-const string strategy_name = "dynamic_compact";
+const string strategy_name = "dynamic_compaction";
 #else
 using Compactor = NaiveCompactor;
-const string strategy_name = "no_compact";
+const string strategy_name = "no_compaction";
 #endif
 
 bool flag_collect_tuples = false;
