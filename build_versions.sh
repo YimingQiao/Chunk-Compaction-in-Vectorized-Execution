55410c6981fac8dedee2cf53ce2f4e3d7e366300
yiqiao <yimingqiao3163@gmail.com>
2024-09-28 19:01:36 +0800

    update the build script

--- build_versions.sh
+++ build_versions.sh
@@ -2,22 +2,33 @@
 
 # Create compaction directory, -p option makes it ignore if the directory already exists
 mkdir -p compaction
+mkdir -p filter_and_join
+
+# Dictionary of all compaction options
+declare -A compaction_options=(
+    ["logical"]="USE_LOGICAL_COMPACT"
+    ["smart"]="USE_SMART_COMPACT")
 
 # Project name - replace with your executable name
 executables=("filter_and_join" "compaction")
 
 for name in "${executables[@]}"; do
-    # Build the no_compact version
-    mkdir -p build-logical
-    cd build-logical
-    # Generate make files with all compaction options off (falls back to no-compact)
-    cmake ..
-    # Build the project
-    make -j96
-    # Move the project
-    mv ${name} ../${name}/exe_logical_${name}
-    # Return to parent directory
-    cd ..
-    rm -rf build-logical
+    for key in "${!compaction_options[@]}"; do
+        # Build the no_compact version
+        mkdir -p build-${key}
+        cd build-${key}
+        # Generate make files with the option enabled
+        cmake -D${compaction_options[$key]}=ON ..
+        # Generate make files with all compaction options off (falls back to no-compact)
+        cmake ..
+        # Build the project
+        make -j96
+        # Move the project
+        mv ${name} ../${name}/exe_${key}_${name}
+        # Return to parent directory
+        cd ..
+        rm -rf build-${key}
+    done
 done
+
 
