092c79eff4526b544e3cc2308bb67fa662e3bc08
yiqiao <yimingqiao3163@gmail.com>
2024-09-28 18:39:41 +0800

    update the build script

--- build_versions.sh
+++ build_versions.sh
@@ -2,6 +2,7 @@
 
 # Create compaction directory, -p option makes it ignore if the directory already exists
 mkdir -p compaction
+mkdir -p filter_and_join
 
 # Dictionary of all compaction options
 declare -A compaction_options=(
@@ -9,34 +10,36 @@ declare -A compaction_options=(
     ["binary"]="USE_BINARY_COMPACT"
     ["dynamic"]="USE_DYNAMIC_COMPACT")
 
-# Project name - replace with your executable name
-project_name="compaction"
+# Define project names
+executables=("filter_and_join" "compaction")
 
 # Loop through all compaction options and compile projects
-for key in ${!compaction_options[@]}; do
-    # Create a unique build directory for each option
-    mkdir -p build-${key}
-    cd build-${key}
-    # Generate make files with the option enabled
-    cmake -D${compaction_options[$key]}=ON ..
+for name in "${executables[@]}"; do
+    for key in "${!compaction_options[@]}"; do
+        # Create a unique build directory for each option
+        mkdir -p build-${key}-${name}
+        cd build-${key}-${name}
+        # Generate make files with the option enabled
+        cmake -D${compaction_options[$key]}=ON ..
+        # Build the project
+        make -j96
+        # Move the project
+        mv ${name} ../${name}/exe_${key}_${name}
+        # Return to parent directory
+        cd ..
+        rm -rf build-${key}-${name}
+    done
+
+    # Build the no_compact version
+    mkdir -p build-no-${name}
+    cd build-no-${name}
+    # Generate make files with all compaction options off (falls back to no-compact)
+    cmake ..
     # Build the project
     make -j96
     # Move the project
-    mv ${project_name} ../compaction/exe_${key}_${project_name}
+    mv ${name} ../${name}/exe_no_${name}
     # Return to parent directory
     cd ..
-    rm -rf build-${key}
+    rm -rf build-no-${name}
 done
-
-# Build the no_compact version
-mkdir -p build-no
-cd build-no
-# Generate make files with all compaction options off (falls back to no-compact)
-cmake ..
-# Build the project
-make -j96
-# Move the project
-mv ${project_name} ../compaction/exe_no_${project_name}
-# Return to parent directory
-cd ..
-rm -rf build-no
